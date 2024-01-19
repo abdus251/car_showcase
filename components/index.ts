ed1728e2d086bb157c81abe82ccdd54a198380cb
abdus251 <abdus251@gmail.com>
2024-01-19 19:21:33 +0600

    project complited successfully

---
+++ components/index.ts
@@ -0,0 +1,25 @@
+"use client";
+
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarCard";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
